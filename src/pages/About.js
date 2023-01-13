45c08f827af0da9f900722ea1e3149564725f143
Iolanta Averyanova <iolantazhiznevskaya@gmail.com>
2023-01-13 15:44:14 +0300

    Upload project

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Лучшее React приложение</h1>
+      <p className="lead">Версия приложения 1.0.42</p>
+    </div>
+  </div>
+  )
+}
+
+
